755bcf019b21f8eda57092fbb8431c3b8d0c1b0a
athenova <aleksey.sergeyev@yandex.com>
2025-03-24 22:49:41 +0300

    Постер в ВК

--- project.py
+++ project.py
@@ -2,6 +2,7 @@ from simple_blogger import Journalist
 from simple_blogger.generators.OpenAIGenerator import OpenAITextGenerator
 from simple_blogger.senders.TelegramSender import TelegramSender
 from simple_blogger.senders.InstagramSender import InstagramSender
+from simple_blogger.senders.VkSender import VkSender
 from datetime import datetime
 
 class Project(Journalist):
@@ -11,7 +12,9 @@ class Project(Journalist):
             text_generator=OpenAITextGenerator(),
             topic_word_limit=100,
             reviewer=TelegramSender(),
-            senders=[TelegramSender(channel_id=f"@the_force_nexus"), InstagramSender(channel_token_name='FORCE_NEXUS_THE_TOKEN')],
+            senders=[TelegramSender(channel_id=f"@the_force_nexus"), 
+                     InstagramSender(channel_token_name='FORCE_NEXUS_THE_TOKEN'),
+                     VkSender(group_id="229821912")],
             **kwargs)
 
     def _get_category_folder(self, task):
@@ -28,5 +31,3 @@ class Project(Journalist):
     
     def _system_prompt(self, task):
         return f"Ты - блогер-эзотерик с 1000000 миллионном подписчиков"
-    
-   
